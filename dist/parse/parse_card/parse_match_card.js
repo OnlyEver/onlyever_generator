cd7e4cb169dea58dd0b0677f900f1acdb27cfba9
Shreedhar <shreedharpandey000@gmail.com>
2024-12-18 17:07:36 +0545

    Parsed match card to newer schema/prompts

--- dist/parse/parse_card/parse_match_card.js
+++ dist/parse/parse_card/parse_match_card.js
@@ -2,17 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ParseMatchCard = void 0;
 class ParseMatchCard {
+    constructor() {
+        this._parseMatchContent = (input) => {
+            const grouped = input.reduce((acc, { left_item, right_item }) => {
+                if (!acc[left_item]) {
+                    acc[left_item] = { left_item, right_item: [] };
+                }
+                acc[left_item].right_item.push(right_item);
+                return acc;
+            }, {});
+            return Object.values(grouped);
+        };
+    }
     parse(cardData) {
         try {
             let content = cardData.card_content;
+            const finalContent = this._parseMatchContent(content);
             let displayTitle = this._generateMatchCardDisplayTitle(content);
             let matchCard = {
                 type: {
                     category: "learning",
                     sub_type: cardData.type,
                 },
-                heading: cardData.card_reference,
-                content: content,
+                heading: cardData.card_content.card_reference,
+                content: finalContent,
                 //  content: cardData.card_content,
                 displayTitle: displayTitle,
                 concepts: cardData.concepts,
@@ -29,7 +42,7 @@ class ParseMatchCard {
         let titles = [];
         let counter = 65;
         for (let data of answers) {
-            let value = data.right_item.join(",");
+            let value = data.right_item;
             let leftData = data.left_item;
             let letter = String.fromCharCode(counter);
             titles.push(`${letter}. ${leftData} -- ${value}`);
@@ -41,14 +54,13 @@ class ParseMatchCard {
     _validateMatch(matchCard) {
         let matches = matchCard.content;
         try {
-            for (let elem of matches) {
-                if (elem.left_item.length > 24 || elem.left_item.length == 0) {
-                    throw Error("Invalid Length of left item ");
-                }
-                else if (elem.right_item[0].length > 24 || elem.right_item[0].length == 0) {
-                    throw Error(" Invalid Length of right item");
-                }
-            }
+            // for(let elem of matches){
+            //     if(elem.left_item.length > 30 || elem.left_item.length == 0){
+            //         throw Error("Invalid Length of left item ");
+            //     }else if(elem.right_item[0].length>30 || elem.right_item[0].length == 0){
+            //         throw Error(" Invalid Length of right item")
+            //     }
+            // }
             return matchCard;
         }
         catch (e) {
--- src/parse/parse_card/parse_match_card.ts
+++ src/parse/parse_card/parse_match_card.ts
@@ -1,9 +1,20 @@
 import { match } from "assert";
 
+type InputItem = {
+  left_item: string;
+  right_item: string;
+};
+
+type OutputItem = {
+  left_item: string;
+  right_item: string[];
+};
+
 export class ParseMatchCard {
   parse(cardData: any) {
     try {
       let content = cardData.card_content;
+      const finalContent =this._parseMatchContent(content);
 
       let displayTitle = this._generateMatchCardDisplayTitle(content);
       let matchCard = {
@@ -11,8 +22,8 @@ export class ParseMatchCard {
           category: "learning",
           sub_type: cardData.type,
         },
-        heading: cardData.card_reference,
-        content: content,
+        heading: cardData.card_content.card_reference,
+        content: finalContent,
         //  content: cardData.card_content,
         displayTitle: displayTitle,
         concepts: cardData.concepts,
@@ -26,11 +37,13 @@ export class ParseMatchCard {
     }
   }
 
+  
+
   _generateMatchCardDisplayTitle(answers: any) {
     let titles: string[] = [];
     let counter = 65;
     for (let data of answers) {
-      let value = data.right_item.join(",");
+      let value = data.right_item;
       let leftData = data.left_item;
       let letter = String.fromCharCode(counter);
       titles.push(`${letter}. ${leftData} -- ${value}`);
@@ -40,13 +53,25 @@ export class ParseMatchCard {
     return displayTitle;
   }
 
+   _parseMatchContent = (input: InputItem[]): OutputItem[] => {
+    const grouped = input.reduce<Record<string, OutputItem>>((acc, { left_item, right_item }) => {
+      if (!acc[left_item]) {
+        acc[left_item] = { left_item, right_item: [] };
+      }
+      acc[left_item].right_item.push(right_item);
+      return acc;
+    }, {});
+  
+    return Object.values(grouped);
+  };
+
   _validateMatch(matchCard: any){
     let matches = matchCard.content;
     try{
         for(let elem of matches){
-            if(elem.left_item.length > 24 || elem.left_item.length == 0){
+            if(elem.left_item.length > 30 || elem.left_item.length == 0){
                 throw Error("Invalid Length of left item ");
-            }else if(elem.right_item[0].length>24 || elem.right_item[0].length == 0){
+            }else if(elem.right_item[0].length>30 || elem.right_item[0].length == 0){
                 throw Error(" Invalid Length of right item")
             }
 
